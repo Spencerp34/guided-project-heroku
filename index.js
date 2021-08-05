45284ba68032358fdca05fc9e0f43b4770e7acb5
Spencerp34 <spencerp34@yahoo.com>
2021-08-05 11:18:50 -0600

    gettingThere

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('web 44 rocks')
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server =express()
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet)
+
+server.get('/', (req, res) => {
+    res.send(`
+        <h1>Web 44 is great!</h1>
+    `)
+})
+
+server.use('*', (req, res, next) =>{
+    res.json({message: 'web 44 is awesome indeed!'})
+})
+
+
+const {port} = require('./config')
+
+server.listen(port, () =>{
+    console.log(`listening on ${port}`)
+})
